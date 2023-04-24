683f6334d119e7d4dbb1074f5f9c76b4325b2e85
Manalkl <manel.kl.mk@gmail.com>
2023-04-24 05:06:32 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -9,100 +9,30 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	if (format == NULL)
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
-	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
 
-}
-/**
- * print_chr - writes the char c in stdout
- * @arguments: input char
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: On success return 1
- */
-int print_chr(va_list arguments, char *buf, unsigned int ibuf)
-{
-	char c;
-
-	c = va_arg(arguments, int);
-	handl_buf(buf, c, ibuf);
-
-	return (1);
-}
-#include "main.h"
-
-/**
- * print_str - writes  string
- * @arguments: string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: 1
- */
-int print_str(va_list arguments, char *buf, unsigned int ibuf)
-{
-	char *str;
-	unsigned int i;
-	char nill[] = "(null)";
-
-	str = va_arg(arguments, char *);
-	if (str == NULL)
-	{
-		for (i = 0; nill[i]; i++)
-			ibuf = handl_buf(buf, nill[i], ibuf);
-		return (6);
-	}
-	for (i = 0; str[i]; i++)
-		ibuf = handl_buf(buf, str[i], ibuf);
-	return (i);
-}
-/**
- * print_prg - writes the character c to stdout
- * @a: input char
- * @buf: buffer pointer
- * @i: index for buffer pointer
- * Return: always success 1
- */
-int print_prg(va_list a __attribute__((unused)), char *buf, unsigned int i)
-{
-	handl_buf(buf, '%', i);
-
-	return (1);
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
