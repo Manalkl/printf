3f75cd94208b11810d6b7a6d58a0d1602b297671
FatimaZahraOukmi <Fatimazahra.oukmi@gmail.com>
2023-04-24 05:15:06 -0700

    print char

---
+++ _print_char.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _Print_char - writes the character c
+ *
+ * @c: The character to print
+ *
+ * Return: always success 1.
+ * 
+ * if  error, -1 is returned.
+ *
+ */
+int _write_char(char c)
+{
+	return (write(1, &c, 1));
+}
