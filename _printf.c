d0f6315c1b194ca32eb5b85e5e33a40a5ec7f112
FatimaZahraOukmi <Fatimazahra.oukmi@gmail.com>
2023-04-26 04:49:20 -0700

    update

--- _printf.c
+++ _printf.c
@@ -37,3 +37,76 @@ int _printf(const char *format, ...)
 	va_end(arg_list);
 	return (printed_chars);
 }
+/**
+ * _printf - custom printf function
+ * @format: format string
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i, count = 0;
+
+    va_start(args, format);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    count += putchar(va_arg(args, int));
+                    break;
+                case 's':
+                    count += printf("%s", va_arg(args, char *));
+                    break;
+                case 'S':
+                    count += print_S(va_arg(args, char *));
+                    break;
+                case '%':
+                    count += putchar('%');
+                    break;
+                default:
+                    putchar('%');
+                    putchar(format[i]);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(format[i]);
+            count++;
+        }
+    }
+
+    va_end(args);
+
+    return (count);
+}
+
+/**
+ * print_S - prints a string with non-printable characters
+ * @str: string to print
+ * Return: number of characters printed
+ */
+int print_S(char *str)
+{
+    int i, count = 0;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] < 32 || str[i] >= 127)
+        {
+            count += printf("\\x%02X", str[i]);
+        }
+        else
+        {
+            count += putchar(str[i]);
+        }
+    }
+
+    return (count);
+}
